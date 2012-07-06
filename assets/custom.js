05979b9fa4c4c4f041a08a73d044011a32d1fc2f
Phillip Gray <pixel.ninjad@gmail.com>
2012-07-06 12:17:47 +1200

    initial

--- assets/custom.js
+++
@@ -1,6 +0,0 @@
-// Your JavaScript Code Goes Here!
-// Example:
-
-jQuery(document).ready(function(){
-	alert("Backend JavaScriptr enabled!");
-});
--- extension.driver.php
+++ extension.driver.php
@@ -1,88 +1,25 @@
 <?php
+	
+	Class extension_language_detection extends Extension{
 
-	class extension_backend_javascriptr extends Extension {
-
-		public function about(){
+		public function getSubscribedDelegates() {
 			return array(
-				'name' => 'Backend JavaScriptr',
-				'version' => '0.1',
-				'release-date' => '2011-04-18',
-				'author' => array(
-					'name' => 'João Barbosa',
-					'website' => 'http://www.joaootavio.com.br',
-					'email' => 'falecom@joaootavio.com.br'
+				array(
+					'page' => '/frontend/',
+					'delegate' => 'FrontendParamsPostResolve',
+					'callback' => 'addParameters'
 				)
 			);
 		}
-
-		public function getSubscribedDelegates(){
-			return array(
-				array(
-					'page' => '/backend/',
-					'delegate' => 'AdminPagePreGenerate',
-					'callback' => 'appendMyCode'
-				),
-				array(
-					'page' => '/backend/',
-					'delegate' => 'InitaliseAdminPageHead',
-					'callback' => 'appendCodeMirror'
-				),
-				array(
-					'page' => '/system/preferences/',
-					'delegate' => 'AddCustomPreferenceFieldsets',
-					'callback' => 'appendCodeBox'
-				),
-			);
-		}
-
-		public function appendMyCode($context){
-			
-			// Add the custom code to the head of the page
-			Administration::instance()->Page->addScriptToHead(URL . '/extensions/backend_javascriptr/assets/custom.js', 100, false);
-			
-		}
-		
-		public function appendCodeBox($context){
-		
-			if(isset($_POST['action']['custom_js_save'])){
-				# Path to our file
-				$custom_file = getcwd() . '/extensions/backend_javascriptr/assets/custom.js';
-				# Open the file and reset it, to recieve the new code
-				$open_file = fopen($custom_file, 'w');
-				# Write it, then close
-				fwrite($open_file, $_POST['custom_backend_js']);
-				fclose($open_file);
 				
-			}
-
-			$group = new XMLElement('fieldset');
-			$group->setAttribute('class', 'settings');
-			$group->appendChild(new XMLElement('legend', __('Backend JavaScriptr')));
-
-
-			$div = new XMLElement('div', NULL, array('class' => 'label'));
-			$span = new XMLElement('span', NULL, array('class' => 'frame'));
-
-			$span->appendChild(new XMLElement('button', __('Save'), array('name' => 'action[custom_js_save]', 'type' => 'submit')));
+		public function addParameters($context) {
 			
-			$label = Widget::Label(__('Your JavaScript goes here:'));
-			
-			# Retrieve the stored code to put it inside the textarea
-			$custom_js_content = file_get_contents(getcwd() . '/extensions/backend_javascriptr/assets/custom.js');
+//			$session[
+//			
+//        	$context['params']['ua-latitude'] = $location['geoplugin_latitude'];
+//        	$context['params']['ua-longitude'] = $location['geoplugin_longitude'];
+//        	$context['params']['ua-country'] = $location['geoplugin_countryName'];
+        }
 
-			$label->appendChild(Widget::Textarea('custom_backend_js', 10, 50, $custom_js_content, array('id' => 'backend_javascriptr_field') ));
-			
-			$div->appendChild($label);
-			$div->appendChild($span);
-
-			$div->appendChild(new XMLElement('p', __('Remember: The "extensions/backend_javascriptr/assets/custom.js" has to have all permissions ( chmod 777 ).'), array('class' => 'help')));
-
-			$group->appendChild($div);
-			$context['wrapper']->appendChild($group);
-		}
-		
-		public function appendCodeMirror($context){
-			#Future...
-		}
-		
 	}
+?>
