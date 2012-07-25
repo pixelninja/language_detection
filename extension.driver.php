33d3f0a45e11e93196b4c27906f0aff15856523a
Phillip Gray <pixel.ninjad@gmail.com>
2012-07-25 16:50:53 +1200

    set after unset

--- extension.driver.php
+++ extension.driver.php
@@ -14,6 +14,8 @@
 				
 		public function addParameters($context) {
 			
+			//code list http://www.metamodpro.com/browser-language-codes
+			
 			session_start();
 			
 			if (!isset($_SESSION['language'])) {
@@ -21,7 +23,10 @@
 			}
 			
 			if ($_GET['set_language_code']) $_SESSION['language'] = $_GET['set_language_code'];
-			if ($_GET['clear_language_code']) unset($_SESSION['language']);
+			if ($_GET['clear_language_code']) {
+				unset($_SESSION['language']);
+				$_SESSION['language'] = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+			}
 			
 			
         	$context['params']['language'] = $_SESSION['language'];
